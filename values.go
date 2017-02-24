cb028857cce319e5059e1d3eed4137f03a047563
Krupa Patel <KPATEL@forsythe.com>
2017-02-24 12:28:02 -0600

    added values

---
+++ values.go
@@ -0,0 +1,21 @@
+// learning values in go
+package main
+
+import "fmt"
+
+func main() {
+
+	// strings can be added together
+	fmt.Println("krupa" + "patel")
+
+	//integers
+	fmt.Println("1+1 =", 1+1)
+
+	//floats
+	fmt.Println("7.0/3.0 =", 7.0/3.0)
+
+	//booleans, with boolean operators
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
